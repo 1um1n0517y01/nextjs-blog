e3978515ce0df8bc6d6386a4392b7a2d46aabdbc
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-05-02 19:37:21 +0200

    Added posts; Updated description

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,10 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello!</p>
+        <p>Hello! Check this blog for some info about Next.js</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
+          This is a sample website - you’ll be building a site like this on{' '}
+          <a href='https://nextjs.org/learn'>Next.js tutorial</a>.
         </p>
       </section>
 
--- posts/dynamic-api-routes.md
+++ posts/dynamic-api-routes.md
@@ -3,7 +3,7 @@ title: 'Dynamic API Routes in Next.js'
 date: '2023-05-03'
 ---
 
-API routes support dynamic routes, and follow the same file naming rules used for pages.
+API routes support **dynamic routes**, and follow the same file naming rules used for pages.
 
 For example, the API route _`pages/api/post/[pid].js`_ has the following code:
 
