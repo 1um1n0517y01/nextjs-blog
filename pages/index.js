35a56b0a00c54b6bce9157b24bea072d7a6b648c
1um1n0517y01 <34325214+1um1n0517y01@users.noreply.github.com>
2023-05-03 03:02:38 +0200

    Cleand up index interface paragraph

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,10 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello! Check this blog for info about Next.js</p>
-        <p>
+        {/* <p>
           This is a sample website - you’ll be building a site like this on{' '}
           <a href='https://nextjs.org/learn'>Next.js tutorial</a>.
-        </p>
+        </p> */}
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
